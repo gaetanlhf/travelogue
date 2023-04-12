e8e55fead2b887c045fb4f2eaa37e5a536314db1
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-04-12 14:56:33 +0200

    Login and sign up implemented

--- app/src/main/java/fr/insset/ccm/m1/sag/travelogue/MainActivity.java
+++ app/src/main/java/fr/insset/ccm/m1/sag/travelogue/MainActivity.java
@@ -2,13 +2,49 @@ package fr.insset.ccm.m1.sag.travelogue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //todo
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            //todo
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            //todo
+        }
+    }
+
+    public void onClickStart(View view) {
+        Intent loginActivity = new Intent(this, LoginActivity.class);
+        startActivity(loginActivity);
+        overridePendingTransition(0, 0);
     }
 }
