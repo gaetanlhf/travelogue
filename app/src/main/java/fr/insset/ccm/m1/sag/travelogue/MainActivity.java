1f6a324ffcb80978188f6d4ce8bd2e19382ca5c9
SharonnZ <sharonnelfride@gmail.com>
2023-04-12 16:11:02 +0200

    MainActivity to default

--- app/src/main/java/fr/insset/ccm/m1/sag/travelogue/MainActivity.java
+++ app/src/main/java/fr/insset/ccm/m1/sag/travelogue/MainActivity.java
@@ -1,24 +1,14 @@
 package fr.insset.ccm.m1.sag.travelogue;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private static int autoSaveMomentTimer = 30;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public int getAutoSaveMomentTimer() {
-        return MainActivity.autoSaveMomentTimer;
-    }
-
-    public static void setAutoSaveMomentTimer(int autoSaveMomentTimer) {
-        MainActivity.autoSaveMomentTimer = autoSaveMomentTimer;
     }
 }
