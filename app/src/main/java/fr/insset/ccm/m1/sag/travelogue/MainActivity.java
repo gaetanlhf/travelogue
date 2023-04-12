8632a1145a55632e6f8af2ec6623b05608399229
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-04-12 09:51:07 +0200

    Init project with Android Studio

---
+++ app/src/main/java/fr/insset/ccm/m1/sag/travelogue/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.insset.ccm.m1.sag.travelogue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
