81d91e06a131b0f6105d064d9d04ef518a5ca7ef
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-04-13 09:39:40 +0200

    Add reset password activity

---
+++ app/src/main/java/fr/insset/ccm/m1/sag/travelogue/ForgotPasswordActivity.java
@@ -0,0 +1,71 @@
+package fr.insset.ccm.m1.sag.travelogue;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    private TextView email;
+
+    private ProgressBar spinner;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        spinner = (ProgressBar) findViewById(R.id.forgot_password_spinner);
+        spinner.setVisibility(View.GONE);    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    public void onClickResetPassword(View view) {
+        email = findViewById(R.id.email_forgot_password_textview);
+        if (!TextUtils.isEmpty(email.getText().toString())) {
+            spinner.setVisibility(View.VISIBLE);
+            FirebaseAuth.getInstance().sendPasswordResetEmail(email.getText().toString())
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), getResources().getString(R.string.forgot_password_success),
+                                    Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            spinner.setVisibility(View.GONE);
+                            Toast.makeText(getApplicationContext(), getResources().getString(R.string.forgot_password_failed),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+}
