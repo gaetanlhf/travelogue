a71c5bed426d335714f154f4542def687678f456
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-05-25 19:17:54 +0200

    Update PermissionHelper.java

--- app/src/main/java/fr/insset/ccm/m1/sag/travelogue/helper/PermissionHelper.java
+++ app/src/main/java/fr/insset/ccm/m1/sag/travelogue/helper/PermissionHelper.java
@@ -10,6 +10,7 @@ import androidx.core.content.ContextCompat;
 public class PermissionHelper {
 
     private static final int PERMISSION_REQUEST_CODE = 100;
+    private static final int BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE = 101;
 
     private static String[] permissions = {
             Manifest.permission.ACCESS_COARSE_LOCATION,
@@ -19,30 +20,35 @@ public class PermissionHelper {
 
     public static void verifyPermissions(Activity activity) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            permissions = appendArray(permissions, Manifest.permission.ACCESS_BACKGROUND_LOCATION);
-        }
-
-        for (String permission : permissions) {
-            if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(activity);
-                return;
+            for (String permission : permissions) {
+                if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(activity);
+                    return;
+                }
+            }
+            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestBackgroundLocationPermission(activity);
+            }
+        } else {
+            for (String permission : permissions) {
+                if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(activity);
+                    return;
+                }
             }
         }
-    }
-
-    private static String[] appendArray(String[] array, String item) {
-        String[] newArray = new String[array.length + 1];
-        System.arraycopy(array, 0, newArray, 0, array.length);
-        newArray[newArray.length - 1] = item;
-        return newArray;
     }
 
     private static void requestPermissions(Activity activity) {
         ActivityCompat.requestPermissions(activity, permissions, PERMISSION_REQUEST_CODE);
     }
 
+    private static void requestBackgroundLocationPermission(Activity activity) {
+        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE);
+    }
+
     public static boolean arePermissionsGranted(int requestCode, String[] permissions, int[] grantResults) {
-        if (requestCode == PERMISSION_REQUEST_CODE) {
+        if (requestCode == PERMISSION_REQUEST_CODE || requestCode == BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE) {
             for (int result : grantResults) {
                 if (result != PackageManager.PERMISSION_GRANTED) {
                     return false;
@@ -56,6 +62,11 @@ public class PermissionHelper {
     public static boolean areAllPermissionsGranted(Activity activity) {
         for (String permission : permissions) {
             if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 return false;
             }
         }
