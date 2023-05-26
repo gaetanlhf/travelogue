51253e13df7a1ea3795db17cb790cb1c6d1865ee
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-05-26 10:24:51 +0200

    Update PermissionHelper.java

--- app/src/main/java/fr/insset/ccm/m1/sag/travelogue/helper/PermissionHelper.java
+++ app/src/main/java/fr/insset/ccm/m1/sag/travelogue/helper/PermissionHelper.java
@@ -2,49 +2,81 @@ package fr.insset.ccm.m1.sag.travelogue.helper;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
+
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.util.Arrays;
 
 public class PermissionHelper {
 
     private static final int PERMISSION_REQUEST_CODE = 100;
     private static final int BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE = 101;
 
-    private static String[] permissions = {
+    private static final String[] permissions = {
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.CAMERA
     };
 
+    private static final String[] backgroundLocationPermission = {
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+    };
+
     public static void verifyPermissions(Activity activity) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            for (String permission : permissions) {
-                if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(activity);
-                    return;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            if (!areAllPermissionsGranted(activity, permissions)) {
+                requestPermissions(activity, permissions, PERMISSION_REQUEST_CODE);
+            } else if (!areAllPermissionsGranted(activity, backgroundLocationPermission)) {
+                if (shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                    showAlertAndOpenAppSettings(activity);
+                } else {
+                    requestPermissions(activity, backgroundLocationPermission, BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE);
                 }
             }
-            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                requestBackgroundLocationPermission(activity);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (!areAllPermissionsGranted(activity, concatAll(permissions, backgroundLocationPermission))) {
+                requestPermissions(activity, concatAll(permissions, backgroundLocationPermission), PERMISSION_REQUEST_CODE);
             }
         } else {
-            for (String permission : permissions) {
-                if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(activity);
-                    return;
-                }
+            if (!areAllPermissionsGranted(activity, permissions)) {
+                requestPermissions(activity, permissions, PERMISSION_REQUEST_CODE);
             }
         }
     }
 
-    private static void requestPermissions(Activity activity) {
-        ActivityCompat.requestPermissions(activity, permissions, PERMISSION_REQUEST_CODE);
+
+    public static void showAlertAndOpenAppSettings(Activity activity) {
+        new MaterialAlertDialogBuilder(activity)
+                .setTitle("Background Location Permission Needed")
+                .setMessage("This app requires the Background Location permission to do its job. Please grant this permission in app settings.")
+                .setPositiveButton("Open Settings", (dialog, which) -> {
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", activity.getPackageName(), null);
+                    intent.setData(uri);
+                    activity.startActivity(intent);
+                })
+                .setNegativeButton("Cancel", (dialog, which) -> {
+                    activity.finish();
+                })
+                .create()
+                .show();
     }
 
-    private static void requestBackgroundLocationPermission(Activity activity) {
-        ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, BACKGROUND_LOCATION_PERMISSION_REQUEST_CODE);
+    public static boolean shouldShowRequestPermissionRationale(Activity activity, String permission) {
+        return ActivityCompat.shouldShowRequestPermissionRationale(activity, permission);
+    }
+
+    private static void requestPermissions(Activity activity, String[] permissionsToRequest, int requestCode) {
+        ActivityCompat.requestPermissions(activity, permissionsToRequest, requestCode);
     }
 
     public static boolean arePermissionsGranted(int requestCode, String[] permissions, int[] grantResults) {
@@ -59,17 +91,37 @@ public class PermissionHelper {
         return false;
     }
 
-    public static boolean areAllPermissionsGranted(Activity activity) {
-        for (String permission : permissions) {
+    public static boolean areAllPermissionsGranted(Activity activity, String[] permissionsToCheck) {
+        for (String permission : permissionsToCheck) {
             if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
                 return false;
             }
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return false;
-            }
+        return true;
+    }
+
+    public static boolean areAllBasicPermissionsGranted(Activity activity) {
+        return areAllPermissionsGranted(activity, permissions);
+    }
+
+    public static boolean isBackgroundLocationPermissionGranted(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            return areAllPermissionsGranted(activity, backgroundLocationPermission);
         }
         return true;
+    }
+
+    private static String[] concatAll(String[] first, String[]... rest) {
+        int totalLength = first.length;
+        for (String[] array : rest) {
+            totalLength += array.length;
+        }
+        String[] result = Arrays.copyOf(first, totalLength);
+        int offset = first.length;
+        for (String[] array : rest) {
+            System.arraycopy(array, 0, result, offset, array.length);
+            offset += array.length;
+        }
+        return result;
     }
 }
