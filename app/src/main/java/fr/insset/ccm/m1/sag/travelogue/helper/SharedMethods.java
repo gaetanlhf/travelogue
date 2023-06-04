81026a0b31f98066f5021c37d554a1c203effef0
Sharonn <sharonnelfride@gmail.com>
2023-06-04 11:14:11 +0200

    deletion of file not used

--- app/src/main/java/fr/insset/ccm/m1/sag/travelogue/helper/SharedMethods.java
+++ app/src/main/java/fr/insset/ccm/m1/sag/travelogue/helper/SharedMethods.java
@@ -1,18 +1,8 @@
 package fr.insset.ccm.m1.sag.travelogue.helper;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.util.Log;
 import android.widget.Toast;
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.json.gson.GsonFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import fr.insset.ccm.m1.sag.travelogue.R;
 
 public class SharedMethods {
     public static void displayToast(Context context, String s) {
@@ -25,11 +15,5 @@ public class SharedMethods {
 
     public static void displayDebugLogMessage(String tag, String message) {
         Log.d(tag, message);
-    }
-
-    public static GoogleClientSecrets getClientSecrets(Resources resources) throws IOException {
-        InputStream inputStream = resources.openRawResource(R.raw.server_client_secret);
-        return GoogleClientSecrets.load(
-                GsonFactory.getDefaultInstance(), new InputStreamReader(inputStream));
     }
 }
